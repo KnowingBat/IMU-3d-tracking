ea67e84db3b1f24fbf701b69bf3b6f7631437f46
Davide D'Angelo <75283094+KnowingBat@users.noreply.github.com>
2022-03-27 18:00:13 +0200

    Update main.js

--- Sketch/main.js
+++ Sketch/main.js
@@ -1,13 +1,18 @@
-let h = 720;
-let w = 1366;
+// Dichiarazione variabili globali
+
+//let h = 720;
+//let w = 1366;
+// Dimensioni del box
 let h_box = 75;
 let w_box = 150;
 let d_box = 200;
+// Lunghezza assi disegnate sul box
 let lAxis = 100;
 let font;
 
+// Variabili per comunicazione seriale e dati in input
 let serial;
-let latestData = "waiting for data";
+let latestData = "no more data";
 let roll, pitch, yaw;
 let str; 
 
@@ -40,20 +45,21 @@ function draw(){
     background(122);
     text(latestData, -100, 200);
     push();
-    //Compute rotation
+    //Effettuo rotazione
     rotateZ(-radians(pitch));
     rotateX(radians(roll));
-    rotateY(radians(yaw));
-    //Draw
+    //rotateY(radians(yaw));
     
+    //Disegno assi sul box
     strokeWeight(6);
     stroke(230,14,14);
-    line(0,0,0,0,0,-lAxis-d_box/2); //X-axis
+    line(0,0,0,0,0,-lAxis-d_box/2); //Asse X
     stroke(21,230,14);
-    line(-lAxis-w_box/2,0, 0 ,0 ,0,0); // Y-axis
-    //line(w_box,h_box, w_box/2,h_box/2);
+    line(-lAxis-w_box/2,0, 0 ,0 ,0,0); // Asse Y
     stroke(7,21,114);
-    line(0, -lAxis-h_box/2, 0, 0, 0, 0); //Z-axis
+    line(0, -lAxis-h_box/2, 0, 0, 0, 0); //asse Z
+    
+    // Disegno box
     strokeWeight(1);
     stroke(0);
     fill(25,122,255);
@@ -62,24 +68,24 @@ function draw(){
 }
 
 function serverConnected() {
- print("Connected to Server");
+    print("Connected to Server");
 }
 
 function gotList(thelist) {
- print("List of Serial Ports:");
+    print("List of Serial Ports:");
 
- for (let i = 0; i < thelist.length; i++) {
-  print(i + " " + thelist[i]);
- }
+    for (let i = 0; i < thelist.length; i++) {
+        print(i + " " + thelist[i]);
+    }
 }
 
 function gotOpen() {
- print("Serial Port is Open");
+    print("Serial Port is Open");
 }
 
 function gotClose(){
- print("Serial Port is Closed");
- latestData = "Serial Port is Closed";
+    print("Serial Port is Closed");
+    latestData = "Serial Port is Closed";
 }
 
 function gotError(theerror) {
@@ -87,17 +93,18 @@ function gotError(theerror) {
 }
 
 function gotData() {
- let currentString = serial.readLine();
-  trim(currentString);
- if (!currentString) return;
- //console.log(currentString);
- latestData = currentString;
-    try {
+    let currentString = serial.readLine();
+    trim(currentString);
+    if (!currentString) return;
+    //console.log(currentString);
+    latestData = currentString;
+/*      try {
         
-    } catch (error) {
+        } catch (error) {
         
-    }
- str = split(latestData, ',');
+        }
+*/    
+    str = split(latestData, ',');
     roll = parseFloat(str[0]);
     pitch = parseFloat(str[1]);
     yaw = parseFloat(str[2]);
